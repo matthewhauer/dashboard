af98a50e54c29eb2e7da5242ff03d5afe8a3e28d
Matt Hauer <matthew.hauer@gmail.com>
2016-06-17 22:25:55 -0700

    Adding 'DeviceStates', need to write it up as an Enumeration. Added stub for Sensor class. Added stubs for 'actuator' and 'drawState' in Device.  Need to figure out the details on those.

--- Device.py
+++ Device.py
@@ -3,6 +3,21 @@ class Device:
         name = ''
         geometry = {}
         sensors = []
+		actuator = None
+		drawState = {
+			posX: 0,
+			posY: 0
+		}
     pass
 
+DeviceStates = ['NOMINAL', 'WARNING', 'ERROR', 'DISABLED',
+	'VALVE-OPEN', 'VALVE-CLOSED', 'CHANGING']
+
+class Sensor:
+	def __init__(self):
+		name = ''
+		connection = {}
+		unit = ''
+		log = {logSize = 0, data = []}
+		
 pass
