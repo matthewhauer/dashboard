17d9a0de5f8482ff0ed746f322990f01141e911b
Matt Hauer <matthew.hauer@gmail.com>
2016-06-21 10:00:40 -0700

    Building component build methods for DeviceLoader.

--- DeviceLoader.py
+++ DeviceLoader.py
@@ -5,13 +5,31 @@ class DeviceLoader:
     def __init__(self):
         pass
 
+    def build_sensor(self, device, the_json):
+        pass
+
+    def build_actuator(self, device, the_json):
+        pass
+
+    def build_device(self, the_json):
+        d = Device()
+        if "name" in the_json:
+            d.name = the_json["name"]
+        if "sensors" in the_json:
+            for sensor in the_json["sensors"]:
+                self.build_sensor(d, the_json)
+        if "actuator" in the_json:
+            self.build_actuator(d, the_json)
+        return d
+
     def load_file(self, filepath):
         the_file = open(filepath, 'r')
         the_json = json.load(the_file)
         # should convert the_json into an actual object....
         devices = []
         for device in the_json:
-            devices.append(Device())
+            if '__type__' in device and device['__type__'] is "Device":
+                devices.append(self.build_device(device))
             pass
         return devices
 
