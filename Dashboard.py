24d35244d2ba165713a4c8e16530f231975f8f42
Matt Hauer <matthew.hauer@gmail.com>
2016-06-21 09:05:47 -0700

    'Qt Hello World' in the Dashboard.  Added lines to load the example devices JSON file.

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,23 @@
-class Device:
-	def __init__(self):
-		self.name = 'my dashboard name'
-	pass
-pass
+import sys
+from PyQt5.QtWidgets import *
+from Device import Device
+from DeviceLoader import DeviceLoader
 
+
+def run_dashboard():
+    app = QApplication(sys.argv)
+    win = QWidget()
+
+    loader = DeviceLoader()
+    theJson = loader.load_file("exampleDevices.json")
+
+    win.setGeometry(100, 100, 800, 600)
+    win.setWindowTitle("LFETS Dashboard")
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    run_dashboard()
+
+#EOF
