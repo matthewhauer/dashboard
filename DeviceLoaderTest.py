ec03fcb81e98137170246141b16aab2afd6f1673
Matt Hauer <matthew.hauer@gmail.com>
2016-06-21 14:11:22 -0700

    Added tests that the sensor count is correct, and that the actuator name is as expected.

--- DeviceLoaderTest.py
+++ DeviceLoaderTest.py
@@ -8,10 +8,16 @@ class TestDeviceLoader(unittest.TestCase):
         service = DeviceLoader()
         device_list = service.load_file("exampleDevices.json")
         self.assertIsNotNone(device_list)
-        self.assertEqual(len(device_list), 3, msg="Incorrect number of devices")
+        self.assertEqual(len(device_list), 3, msg = "Incorrect number of devices")
         for dev_key in device_list.keys():
             device = device_list[dev_key]
-            self.assertTrue(isinstance(device, Device), msg="Device list item not a Device instance")
+            self.assertTrue(isinstance(device, Device), msg = "Device list item not a Device instance")
+        # test "LOX Vent" - two sensors, one actuator
+        lox_vent = device_list["LOX Vent"]
+        self.assertIsNotNone(lox_vent, msg = "Could not find 'LOX Vent' device")
+        self.assertEquals(len(lox_vent.sensors), 2, msg = "Incorrect sensor count in 'LOX Vent'")
+        self.assertIsNotNone(lox_vent.actuator, msg = "No actuator in 'LOX Vent'")
+        self.assertEquals(lox_vent.actuator["name"], "LOX Vent Valve", msg = "LOX Vent device actuator name incorrect")
     pass
 
 
