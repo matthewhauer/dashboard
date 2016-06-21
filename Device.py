4a8255673c6a5ea69054650c7c05d99748cf5e55
Matt Hauer <matthew.hauer@gmail.com>
2016-06-21 09:34:00 -0700

    Doing more proper class setup for Device. Fixing indentation.

--- Device.py
+++ Device.py
@@ -1,25 +1,34 @@
 class Device:
-	def __init__(self):
-		name = ''
-		geometry = {}
-		sensors = []
-		actuator = None
-		draw_state = {}
-		draw_state.pos_x = 0
-		draw_state.pos_y = 0
+    sensors = []
+    draw_state = {}
+    actuator = None
+
+    def __init__(self):
+        name = ''
+        geometry = {}
+        self.draw_state["pos_x"] = 0
+        self.draw_state["pos_y"] = 0
+
+    def add_sensor(self, the_sensor):
+        if the_sensor is not None:
+            self.sensors.append(the_sensor)
+
+    def get_sensor(self, sensor_name):
+        return self.sensors[sensor_name]
 
 
 DeviceStates = ['NOMINAL', 'WARNING', 'ERROR', 'DISABLED',
-	'VALVE-OPEN', 'VALVE-CLOSED', 'CHANGING']
+    'VALVE-OPEN', 'VALVE-CLOSED', 'CHANGING']
+
 
 class Sensor:
-	def __init__(self):
-		name = ''
-		connection = {}
-		unit = ''
-		log = {}
-		log.log_size = 0
-		log.data = []
+    def __init__(self):
+        name = ''
+        connection = {}
+        unit = ''
+        log = {}
+        log.log_size = 0
+        log.data = []
 
 # EOF
 
