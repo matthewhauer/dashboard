cf101db3144d3073611cddd1b4424d81d25c1532
Matt Hauer <matthew.hauer@gmail.com>
2016-07-01 10:02:23 -0700

    no message

--- DeviceLoaderTest.py
+++ DeviceLoaderTest.py
@@ -1,7 +1,9 @@
 import unittest
 from DeviceLoader import DeviceLoader
 from Device import Device
-
+from DevicePanel import DevicePanel
+from PyQt5.QtWidgets import *
+import sys
 
 class TestDeviceLoader(unittest.TestCase):
     def test_example_file(self):
@@ -20,6 +22,22 @@ class TestDeviceLoader(unittest.TestCase):
         self.assertEquals(lox_vent.actuator["name"], "LOX Vent Valve", msg = "LOX Vent device actuator name incorrect")
     pass
 
+    def test_device_panel(self):
+        app = QApplication(sys.argv)
+        win = QWidget()
+
+        loader = DeviceLoader()
+        devices = loader.load_file("exampleDevices.json")
+        lox_vent = devices["LOX Vent"]
+        self.assertIsNotNone(lox_vent, msg="Could not find 'LOX Vent' object")
+        lox_pane = DevicePanel().set_device(lox_vent)
+
+        win.setGeometry(100, 100, 800, 600)
+        win.setWindowTitle("LFETS Dashboard")
+        win.addWidget(lox_pane.get_panel())
+
+        win.show()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
     unittest.main()
--- DevicePanel.py
+++ DevicePanel.py
@@ -2,6 +2,32 @@ from Device import Device
 from PyQt5.QtWidgets import *
 
 class DevicePanel:
-    pass
+    def __init__(self):
+        self.device = None
+        self.widget = None
+        self.layout = None
+
+    def set_device(self, dev):
+        widget = QWidget()
+        layout = QFormLayout()
+        # name
+        layout.addRow(QLabel("Name"), QLabel(dev.name))
+        # sensor count
+        layout.addRow(QLabel("Sensor Ct"), QLabel(str(len(dev.sensors))))
+        # actuator?
+        if dev.actuator is not None:
+            layout.addRow(QLabel("Actuator?"), QLabel("True"))
+        else:
+            layout.addRow(QLabel("Actuator?"), QLabel("False"))
+        # logger? -- loggers are per sensor/actuator, not device.
+        # if dev.logger is not None:
+        #     layout.addRow(QLabel("Actuator?"), QLabel("True"))
+        # else:
+        #     layout.addRow(QLabel("Actuator?"), QLabel("True"))
+        widget.setLayout(layout)
+        return self # builder pattern
+
+    def get_panel(self):
+        return self.widget
 
 # EOF
